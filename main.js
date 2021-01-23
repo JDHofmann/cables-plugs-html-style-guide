4a18c0d08688bff1ec1ca439e5edbdb14a799d14
jdhofmann <justindhofmann@gmail.com>
2021-01-23 17:47:25 -0500

    burger good, menu slides out

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 console.log("loaded js")
 
+let burgerBtn = document.querySelector(".burger-btn")
+
+let nav = document.querySelector("nav")
+
+burgerBtn.addEventListener( "click", () => {
+    nav.classList.remove("nav-hidden")
+} )
+
 // tasks
-// build nav bar
-// build burger menu 
+// √ build nav bar
+// √ build burger menu 
 // build header link carousel
 // 
