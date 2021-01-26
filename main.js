1f6d52a5f68c80ea6699fc7d6ab65d9a45587677
jdhofmann <justindhofmann@gmail.com>
2021-01-26 13:08:30 -0500

    tried scrolling, that is not smart

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ burgerBtn.addEventListener( "click", () => {
     nav.classList.remove("nav-hidden")
 } )
 
+function scrollIt(){
+    console.log("scrolling")
+    let item2 = document.getElementById("2")
+    item2.scrollIntoView()
+}
+
+setTimeout( scrollIt, 2000 )
 // tasks
 // √ build nav bar
 // √ build burger menu 
